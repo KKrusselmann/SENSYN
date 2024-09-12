2f281317a82e43328ab882c54f134057c33f5d8e
Katharina Krusselmann <katharina.krusselmann@gmail.com>
2024-09-12 11:19:13 +0300

    add reference to synthetic data

--- WebApp/pages/3_Explore_Homicide_Data.py
+++ WebApp/pages/3_Explore_Homicide_Data.py
@@ -313,7 +313,7 @@ with tab2:
     #Renaming of variable labels for accessibility purposes.
     filtered_DHM.rename(columns=label_map, inplace=True)
     st.write(filtered_DHM)
-    st.wite("When downloading and reusing the data, please reference the dataset as Krüsselmann, K., Achterberg, J., Haas, M., Spruit, M. & Liem, M. (2024). Synthetic Dutch Homicide Monitor [Data set]. Leiden University/Leiden University Medical Center. (https://doi.org/10.5281/zenodo.13378063)")
+    st.write("When downloading and reusing the data, please reference the dataset as Krüsselmann, K., Achterberg, J., Haas, M., Spruit, M. & Liem, M. (2024). Synthetic Dutch Homicide Monitor [Data set]. Leiden University/Leiden University Medical Center. (https://doi.org/10.5281/zenodo.13378063)")
 
     with tab3:
         st.write("""
